3c3e1285a8156ff8cd9c2c8f23687000217a87fd
pietrochies <pietro.chies@edu.pucrs.br>
2025-10-08 21:52:40 +0000

    UC3 UC4

---
+++ src/main/java/com/chiespietro/ex4_lancheriaddd_v1/Aplicacao/SolicitarStatusPedidoUC.java
@@ -0,0 +1,21 @@
+package com.chiespietro.ex4_lancheriaddd_v1.Aplicacao;
+
+import com.chiespietro.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
+
+public class SolicitarStatusPedidoUC {
+    
+
+    public StatusPedidoResponse executar(Pedido pedido) {
+        return new StatusPedidoResponse(pedido.getId(), pedido.getStatus().name());
+    }
+
+    public static class StatusPedidoResponse {
+        public final long pedidoId;
+        public final String status;
+
+        public StatusPedidoResponse(long pedidoId, String status) {
+            this.pedidoId = pedidoId;
+            this.status = status;
+        }
+    }
+}
