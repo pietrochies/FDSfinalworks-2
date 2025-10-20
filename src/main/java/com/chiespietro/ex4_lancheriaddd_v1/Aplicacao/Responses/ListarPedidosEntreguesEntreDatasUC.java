431bd7422a9570b52d26026a9e36e845063f512a
pietrochies <pietro.chies@edu.pucrs.br>
2025-10-20 20:08:28 +0000

    UC6

---
+++ src/main/java/com/chiespietro/ex4_lancheriaddd_v1/Aplicacao/Responses/ListarPedidosEntreguesEntreDatasUC.java
@@ -0,0 +1,32 @@
+package com.chiespietro.ex4_lancheriaddd_v1.Aplicacao;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.chiespietro.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
+import com.chiespietro.ex4_lancheriaddd_v1.Dominio.Repositorios.PedidoRepository;
+
+public class ListarPedidosEntreguesEntreDatasUC {
+
+    private final PedidoRepository pedidoRepository;
+
+    public ListarPedidosEntreguesEntreDatasUC(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public List<Pedido> executar(LocalDateTime inicio, LocalDateTime fim) { // depois mudar para LocalDate
+        if (inicio == null || fim == null) {
+            throw new IllegalArgumentException("As datas de início e fim são obrigatórias."); 
+        }
+
+        if (fim.isBefore(inicio)) {
+            throw new IllegalArgumentException("A data final não pode ser anterior à data inicial."); // depois mudar para LocalDate
+        }
+
+        return pedidoRepository.findByStatusAndDataEntregaBetween(
+            Pedido.Status.ENTREGUE,
+            inicio,
+            fim
+        );
+    }
+}
